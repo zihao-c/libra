3110221ee605f95b661318a2abb92d279f9e0a33
zihaoccc <zihaoccc@users.noreply.github.com>
2021-03-14 05:40:45 +0000

    deploy: 76c8fba3a34bdea8779a1ec10f38a0600d40af09

---
+++ diem_types/account_config/resources/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["account",""],["balance",""],["chain_id",""],["currency_info",""],["designated_dealer",""],["dual_attestation",""],["freezing_bit",""],["key_rotation_capability",""],["preburn_balance",""],["preburn_queue",""],["role",""],["role_id",""],["vasp",""],["withdraw_capability",""]]});
